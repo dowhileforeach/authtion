9cc6464c98ce35a3e954a8f351e577aaa30425b7
z <alex@dwfe.ru>
2018-04-07 03:48:16 +0300

    mailing

---
+++ src/main/java/ru/dwfe/net/authtion/schedule/ScheduledTasks.java
@@ -0,0 +1,44 @@
+package ru.dwfe.net.authtion.schedule;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ru.dwfe.net.authtion.dao.repository.MailingNewConsumerPasswordRepository;
+
+@Component
+public class ScheduledTasks
+{
+    private final static Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Autowired
+    MailingNewConsumerPasswordRepository mailingNewConsumerPasswordRepository;
+
+    @Scheduled(fixedDelay = 30_000)
+    public void mailingHighPriority()
+    {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo("pistoletik@gmail.com");
+        message.setFrom("noreply@dwfe.ru");
+        message.setSubject("Welcome");
+        message.setText("Welcome to\nDWFE.ru");
+        emailSender.send(message);
+        log.info("sended");
+
+//        mailingNewConsumerPasswordRepository.findAll().forEach(next -> {
+//
+//        });
+    }
+
+    @Scheduled(fixedDelay = 35_000)
+    public void mailingStandardPriority()
+    {
+
+    }
+}
