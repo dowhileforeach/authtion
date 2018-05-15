606834637b311e1b5563e071e25d2ce2e45582e1
z <alex@dwfe.ru>
2018-05-16 01:41:11 +0300

    refact

--- src/test/resources/sql/data-test.sql
+++ src/test/resources/sql/data-test.sql
@@ -4,13 +4,13 @@ USE authtion_test;
 # Password for consumer test2@dwfe.ru = test22
 LOCK TABLES authtion_consumers WRITE, authtion_authorities WRITE, authtion_consumer_authority WRITE;
 INSERT INTO authtion_consumers VALUES
-  (1, 'test1@dwfe.ru', '{bcrypt}$2a$10$cWUX5MiFl8rJFVxKxEbON.2QcJ/0RsVfhVvvqDG5wEOM/bstMIk6m', 'test1', '', '', 1, 1, 1, 1, 1, '2017-07-07 07:07:07', '2017-07-07 07:07:07'),
-  (555, 'test2@dwfe.ru', '{bcrypt}$2a$10$9SCLBifjy2Ieaoc6VLmSgOQsxf4NUlbGO32zMraftTXcl3jEAqlbm', 'test2', '', '', 1, 1, 1, 1, 1, '2017-07-07 07:07:07', '2017-07-07 07:07:07');
+  (1000, 'test1@dwfe.ru', '{bcrypt}$2a$10$cWUX5MiFl8rJFVxKxEbON.2QcJ/0RsVfhVvvqDG5wEOM/bstMIk6m', 'test1', '', '', 1, 1, 1, 1, 1, '2017-07-07 07:07:07', '2017-07-07 07:07:07'),
+  (1001, 'test2@dwfe.ru', '{bcrypt}$2a$10$9SCLBifjy2Ieaoc6VLmSgOQsxf4NUlbGO32zMraftTXcl3jEAqlbm', 'test2', '', '', 1, 1, 1, 1, 1, '2017-07-07 07:07:07', '2017-07-07 07:07:07');
 INSERT INTO authtion_authorities VALUES
   ('ADMIN', 'Administrator'),
   ('USER', 'Standard consumer');
 INSERT INTO authtion_consumer_authority VALUES
-  (1, 'ADMIN'),
-  (1, 'USER'),
-  (555, 'USER');
+  (1000, 'ADMIN'),
+  (1000, 'USER'),
+  (1001, 'USER');
 UNLOCK TABLES;
