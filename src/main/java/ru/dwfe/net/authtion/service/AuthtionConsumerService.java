b25fc84a963b4827d955c0cef0e19429d78850fb
z <alex@dwfe.ru>
2018-05-21 16:56:26 +0300

    user entity

--- src/main/java/ru/dwfe/net/authtion/service/AuthtionConsumerService.java
+++ src/main/java/ru/dwfe/net/authtion/service/AuthtionConsumerService.java
@@ -1,7 +1,5 @@
 package ru.dwfe.net.authtion.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,19 +18,15 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public class AuthtionConsumerService implements UserDetailsService
 {
-  private static final Logger log = LoggerFactory.getLogger(AuthtionConsumerService.class);
-
   @Autowired
   private AuthtionConsumerRepository repository;
 
   @Override
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException
   {
-    return repository.findByEmail(email).orElseThrow(() -> {
-      String str = String.format("The not exist: %s", email);
-      log.error(str);
-      return new UsernameNotFoundException(str);
-    });
+    return repository.findByEmail(email).orElseThrow(() ->
+            new UsernameNotFoundException(String.format("Username not exist: %s", email))
+    );
   }
 
   public Optional<AuthtionConsumer> findById(Long id)
@@ -52,7 +46,7 @@ public class AuthtionConsumerService implements UserDetailsService
 
   public List<AuthtionConsumer> findAll()
   {
-    return (List<AuthtionConsumer>) repository.findAll();
+    return repository.findAll();
   }
 
   @Transactional
